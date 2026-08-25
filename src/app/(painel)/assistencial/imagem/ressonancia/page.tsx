2b93ef724dfdeb815cc6ab903817c58f277f2b2e
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-08-25 18:50:23 -0300

    feat(imagem): criar modulo de ressonancia

---
+++ src/app/(painel)/assistencial/imagem/ressonancia/page.tsx
@@ -0,0 +1,3 @@
+import { ModalidadeWorklist } from "@/components/imagem/modalidade-worklist";
+export const dynamic="force-dynamic"; export const revalidate=0;
+export default function RessonanciaPage(){return <ModalidadeWorklist modalidade="RM"/>}
