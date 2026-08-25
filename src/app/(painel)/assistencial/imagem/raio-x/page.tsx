251d69b0f50eaec07671ee270106072065e27af0
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-08-25 18:50:04 -0300

    feat(imagem): criar modulo de raio x

---
+++ src/app/(painel)/assistencial/imagem/raio-x/page.tsx
@@ -0,0 +1,3 @@
+import { ModalidadeWorklist } from "@/components/imagem/modalidade-worklist";
+export const dynamic="force-dynamic"; export const revalidate=0;
+export default function RaioXPage(){return <ModalidadeWorklist modalidade="RX"/>}
